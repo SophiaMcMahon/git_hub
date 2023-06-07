a3f31f8749921dd11e767cd6b21ec05abafb276e
Sophia McMahon <112043523+SophiaMcMahon@users.noreply.github.com>
2023-06-07 09:54:19 -0700

    Update Contains_Duplicate.java
    Added another solution using a HashSet rather than Array sorting. However, my original solution yields better runtime.

--- Contains_Duplicate.java
+++ Contains_Duplicate.java
@@ -2,6 +2,7 @@
 class Solution {
 	 public boolean containsDuplicate(int[] nums) {
 	        Arrays.sort(nums); 
+		 // Solving this problem using sorting yields for loglinear complexity.
 	        for (int i = 0; i < nums.length - 1; i++) {
 	                if (nums[i] == nums[i+1]) {
 	                    return true;
@@ -10,3 +11,16 @@ class Solution {
 	        return false;
 	    }
 }
+
+// Another possible solution using HashSet. 
+class Solution {
+	public boolean containsDuplicate(int[] nums) {
+		HashSet<Integer> numbers = new HashSet<>();
+// O(n) space and time.
+		for(int i = 0; i < nums.length; i++) {
+			if(numbers.contains(nums[i])) return true;
+			numbers.add(nums[i]);
+		}
+		return false;
+}
+ }
